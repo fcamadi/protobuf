0fed1bf73f1bf875963a0e21ce90a5972dd57d33
francd <fcamadi@gmail.com>
2022-10-02 22:02:40 +0200

    feat: json vs protobuf performance comparison

---
+++ src/main/java/org/francd/PerformanceTest.java
@@ -0,0 +1,58 @@
+package org.francd;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.francd.json.PersonDto;
+import org.francd.model.Person;
+
+import java.io.IOException;
+
+public class PerformanceTest {
+
+    public static void main(String[] args) {
+
+        //JSON
+        PersonDto jsonPerson = new PersonDto();
+        jsonPerson.setName("JsonPerson");
+        jsonPerson.setAge(44);
+
+        ObjectMapper mapper = new ObjectMapper();
+
+        Runnable runnableJson = () -> {
+            try {
+                byte[] bytes = mapper.writeValueAsBytes(jsonPerson);
+                PersonDto personDtoRead = mapper.readValue(bytes, PersonDto.class);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        };
+
+        //Protobuf
+        Person personProto = Person.newBuilder()
+                .setName("ProtPerson")
+                .setAge(44)
+                .build();
+
+        Runnable runnableProtobuf = () -> {
+            try {
+                byte[] bytes = personProto.toByteArray();
+                Person personProtoRead = Person.parseFrom(bytes);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        };
+
+        runPerformanceTest(runnableProtobuf, "Protobuf");
+        runPerformanceTest(runnableJson, "JSON");
+    }
+
+    public static void runPerformanceTest(Runnable runnable, String method) {
+
+        long startTime = System.currentTimeMillis();
+        for (int i=0; i < 1_000_000; i++) {
+            runnable.run();
+        }
+        long endTime = System.currentTimeMillis();
+
+        System.out.println(method + " : " + (endTime - startTime) + " ms");
+    }
+}
