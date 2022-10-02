46ca757d00bda4f7d26da8f55909e6e20cefb8b1
francd <fcamadi@gmail.com>
2022-10-02 20:04:05 +0200

    feat: serialize and deserialize example

--- src/main/java/org/francd/protobuf/PersonDemo.java
+++ src/main/java/org/francd/protobuf/PersonDemo.java
@@ -2,9 +2,14 @@ package org.francd.protobuf;
 
 import org.francd.model.Person;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class PersonDemo {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         Person fulanito = org.francd.model.Person.newBuilder()
                 .setName("Fulanito")
@@ -12,11 +17,15 @@ public class PersonDemo {
                 .build();
         System.out.println(fulanito);
 
-        Person menganito = org.francd.model.Person.newBuilder()
-                .setName("Fulanito")
-                .setAge(33)
-                .build();
-        System.out.println(fulanito.equals(menganito));
+        //serialize
+        Path path = Paths.get("Fulanito.ser");
+        Files.write(path, fulanito.toByteArray());
+
+        //deserialize
+        byte[] personBytes = Files.readAllBytes(path);
+        Person person = Person.parseFrom(personBytes);
+
+        System.out.println("Deserialized person is the same: "+fulanito.equals(person));
     }
 }
 
