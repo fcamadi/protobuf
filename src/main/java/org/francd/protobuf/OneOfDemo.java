cd7e9ae3a24627831c6548fe6212ae32a95de43d
francd <fcamadi@gmail.com>
2022-10-09 19:41:47 +0200

    refactor: oneof demo

---
+++ src/main/java/org/francd/protobuf/OneOfDemo.java
@@ -0,0 +1,32 @@
+package org.francd.protobuf;
+
+import org.francd.model.common.Credentials;
+import org.francd.model.common.EmailCredentials;
+import org.francd.model.common.PhoneOTP;
+
+public class OneOfDemo {
+
+    public static void main(String[] args) {
+
+        EmailCredentials emailCredentials = EmailCredentials.newBuilder()
+                .setEmail("fulano@gmail.com")
+                .setPassword("admin123")
+                .build();
+
+        PhoneOTP phoneOTP = PhoneOTP.newBuilder()
+                .setNumber(123456789)
+                .setCode(111)
+                .build();
+
+        Credentials credentials = Credentials.newBuilder()
+                .setEmailMode(emailCredentials)
+                .build();
+
+        login(credentials);
+    }
+
+    public static void login(Credentials credentials) {
+        System.out.println(credentials.getEmailMode());
+    }
+
+}
